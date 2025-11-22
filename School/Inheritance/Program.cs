882bae4a7b9f667bdc3f9b31722cff2871ab82e5
upraveenk <upraveenk18@gmail.com>
2025-11-22 15:27:46 +0530

    inheritance

---
+++ School/Inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Class1
+{
+    public Class1()
+    {
+        Console.WriteLine("Class1 Constructor Called");
+    }
+    public void Test1()
+    {
+        Console.WriteLine("First Method");
+    }
+    public void Test2()
+    {
+        Console.WriteLine("Second Method");
+    }
+}
+class Class2 : Class1
+{
+    public void Test3()
+    {
+        Console.WriteLine("Thirst Method");
+    }
+    public Class2()
+    {
+        Console.WriteLine("Class2 Constructor called");
+    }
+    static void Main()
+    {
+        Class2 c = new Class2();
+        c.Test1();
+        c.Test2();
+        c.Test3();
+        Console.ReadLine();
+    }
+}
